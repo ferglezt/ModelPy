abbcb37eefa54abae5f833454acc5631b129a9ea
Fernando <fernando.gonzalez.upiicsa@gmail.com>
2022-04-06 01:17:40 -0500

    Create Category and Subcategory models

--- ModelPy/modelfest2022/models.py
+++ ModelPy/modelfest2022/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	category_id = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=60)
+
+class SubCategory(models.Model):
+	subcategory_id = models.IntegerField()
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	name = models.CharField(max_length=60)
+
+	class Meta:
+		unique_together = (('category', 'subcategory_id'),)
+
