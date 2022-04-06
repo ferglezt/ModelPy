1ce9402ede3063bfe6d1ad8873bad4cdac748814
Fernando <fernando.gonzalez.upiicsa@gmail.com>
2022-04-05 20:45:23 -0500

    Add modelfest2022 migrations

--- ModelPy/ModelPy/settings.py
+++ ModelPy/ModelPy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'modelfest2022.apps.Modelfest2022Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
