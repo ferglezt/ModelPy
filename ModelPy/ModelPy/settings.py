6840e6fb519aa40a14138ecbe631802a4a02f613
Fernando <fernando.gonzalez.upiicsa@gmail.com>
2022-04-05 21:17:03 -0500

    Add simple sidebar template

--- ModelPy/ModelPy/settings.py
+++ ModelPy/ModelPy/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ModelPy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
